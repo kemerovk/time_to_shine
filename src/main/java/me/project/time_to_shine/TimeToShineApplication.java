2235b1e7e64a1ca49dd00fe8385f9dce4c84b784
Tsoy_Kirill <tsoykirya@gmail.com>
2025-03-20 22:49:42 +0300

    установил h2

--- src/main/java/me/project/time_to_shine/TimeToShineApplication.java
+++ src/main/java/me/project/time_to_shine/TimeToShineApplication.java
@@ -3,11 +3,14 @@ package me.project.time_to_shine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class TimeToShineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TimeToShineApplication.class, args);
+        new Scanner(System.in).nextLine();
     }
 
 }
