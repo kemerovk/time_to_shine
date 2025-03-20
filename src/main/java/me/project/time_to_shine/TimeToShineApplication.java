e6b9db856315c4fe2a09411724514da0753f3b64
Tsoy_Kirill <tsoykirya@gmail.com>
2025-03-20 21:58:12 +0300

    first commit

---
+++ src/main/java/me/project/time_to_shine/TimeToShineApplication.java
@@ -0,0 +1,13 @@
+package me.project.time_to_shine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeToShineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeToShineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/project/time_to_shine/TimeToShineApplicationTests.java
@@ -0,0 +1,13 @@
+package me.project.time_to_shine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeToShineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
